1ce82a952a82518c77b8a681bdc03e262938281b
nesmpr <nesmpr@hotmail.com>
2026-04-07 19:24:08 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'meu-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css', // adicione todos os arquivos que quer cachear
+  '/script.js',
+  '/icon-192.png'
+];
+
+// Instalação do service worker e cache dos arquivos
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Intercepta requisições e serve do cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Atualiza o cache quando o service worker é ativado
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
